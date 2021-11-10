ab5febb035881f0024620e6adec46bb0a5808eba
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2021-11-10 18:14:45 +0100

    created a docs folder

---
+++ .history/docs/pages/components/Layout_20211102110220.js
@@ -0,0 +1,9 @@
+const Layout = () => {
+    return (
+        <div>
+            <h1>GENERAL LAYOUT</h1>
+        </div>
+    )
+}
+
+export default Layout
---
+++ docs/pages/components/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return (
+        <div>
+            <h1>NAVIGATION BAR</h1>
+        </div>
+    )
+}
+
+export default Nav
